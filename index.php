e4f67acc0a1fc9a35c8fad144d2b3abf6b38b37a
Cherry098-w <quesito21500@outlook.com>
2025-11-06 16:19:39 -0600

    Cambio index prubea

--- index.php
+++ index.php
@@ -3,3 +3,15 @@
 header("Location: auth/login.php");
 exit; // Finaliza el script después de la redirección
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
