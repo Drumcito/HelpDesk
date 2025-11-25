a5aa5a7d4b2d77caa81ae360fa168a5ad7fc2eae
Drumcito <brandon.asc15@gmail.com>
2025-11-25 12:03:45 -0600

    Se corrigieron colores, se añadio un header ya bien,

--- template/header.php
+++ template/header.php
@@ -7,11 +7,11 @@ if (session_status() === PHP_SESSION_NONE) {
 
 <header class="eqf-header">
     <div class="eqf-header-left">
-        <img src="../assets/img/icon_helpdesk.png" alt="Helpdesk EQF" class="header-icon">
+        <img src="/HELPDESK_EQF/assets/img/icon_helpdesk.png" alt="Helpdesk EQF" class="header-icon">
     </div>
 
 
     <div class="eqf-header-right">
-        <img src="../assets/img/Logo-334x98.png" alt="EQF Logo" class="header-logo">
+        <img src="/HELPDESK_EQF/assets/img/Logo-334x98.png" alt="EQF Logo" class="header-logo">
     </div>
 </header>
